07bba611b126164c27ab65f091f8bc7eaba43e21
dereypl <mpasich.pk@gmail.com>
2020-06-07 11:22:10 +0200

    Routing added.
    + react-router-dom installed
    + AuthForm added
    + Routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {ROUTES} from "./utils/routes";
+import AuthFrom from "./components/views/AuthForm";
 
 const App = () => (
-    <div>Hello CODERS`PEAK!</div>
+    <BrowserRouter>
+        <Switch>
+            <Route exact path={ROUTES.AUTH} component={AuthFrom}/>
+        </Switch>
+    </BrowserRouter>
 );
 
 export default App;
---
+++ src/components/views/AuthForm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const AuthFrom = () => (
+    <div>Hello CODERS`PEAK!</div>
+);
+
+
+export default AuthFrom;
