a233ad468b54331523e9c0a01942572560227779
dereypl <mpasich.pk@gmail.com>
2020-06-07 16:13:50 +0200

    Updates

---
+++ precache-manifest.49acd1435a314a786a2917e03cc91338.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a790728e4c2fa46db5b36cf5e34a78",
+    "url": "/palindrome-checker/index.html"
+  },
+  {
+    "revision": "091f69d78c5a4a24e954",
+    "url": "/palindrome-checker/static/js/2.61d3f50c.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/palindrome-checker/static/js/2.61d3f50c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c313cf2c96f99b73ff",
+    "url": "/palindrome-checker/static/js/main.ed9fda22.chunk.js"
+  },
+  {
+    "revision": "3c97ed5d08903d8b563e",
+    "url": "/palindrome-checker/static/js/runtime-main.ad04bce2.js"
+  }
+]);
