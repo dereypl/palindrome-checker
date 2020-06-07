14fe7d9f947025f83cfb77d98b80e4b12ede6713
dereypl <mpasich.pk@gmail.com>
2020-06-07 18:10:37 +0200

    HashRouter included => gh-pages deployment fixed.
    + BrowserRouter changed to HashRouter
    + REDUX_DEVTOOLS_EXTENSION removed
    + Link to live demo included into readme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {HashRouter, Route, Switch} from "react-router-dom";
 import {ROUTES} from "./utils/routes";
 import AuthFrom from "./components/views/AuthForm";
 import StylesProvider from "./components/ui-config/StylesProvider";
 import PrivateRoute from "./utils/routes/PrivateRoute";
 import Palindromes from "./components/views/Palindromes";
 import {Provider} from "react-redux";
-import configureStore from "./store/configureStore";
+import store from "./store/configureStore";
 
-const store = configureStore();
 
 const App = () => (
     <Provider store={store}>
         <StylesProvider>
-            <BrowserRouter>
+            <HashRouter basename='/'>
                 <Switch>
                     <Route exact path={ROUTES.PUBLIC.AUTH} component={AuthFrom}/>
                     <PrivateRoute exact path={ROUTES.USER.PALINDROMES} component={Palindromes}/>
                 </Switch>
-            </BrowserRouter>
+            </HashRouter>
         </StylesProvider>
     </Provider>
 );
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,7 @@
 import palindromesReducer from "./reducers/palindromesReducer";
-import {applyMiddleware, compose, createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import thunk from 'redux-thunk';
-
 // --- In real app here we should use redux-persist to preserve state on refresh ---
-// --- REDUX_DEVTOOLS_EXTENSION should be removed on production ---
 
-export default () => createStore(
-    palindromesReducer,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
+const store = createStore(palindromesReducer, applyMiddleware(thunk));
+export default store;
