e5df17d4e62b6399efbaf44df8f655029a80c266
dereypl <mpasich.pk@gmail.com>
2020-06-07 11:52:24 +0200

    AuthForm container added.
    + React-hook-form added
    + LoginForm styles added
    + PersonIcon component added and included into LoginForm

---
+++ src/components/icons/PersonIcon.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const PersonIcon = ({width, height}) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 85 85">
+        <g id="Group_71" data-name="MP-GROUP" transform="translate(-689 -677)">
+            <circle id="Ellipse_1" data-name="MP-ECLIPSE" cx="42.5" cy="42.5" r="42.5" transform="translate(689 677)"
+                    fill="#f6f8fa"/>
+            <g id="person-24px_1_" data-name="MP-PERSON" transform="translate(707 695)">
+                <path id="MP-PATH1" data-name="MP-PATH1"
+                      d="M20.054,20.054a8.027,8.027,0,1,0-8.027-8.027A8.025,8.025,0,0,0,20.054,20.054Zm0,4.014C14.7,24.068,4,26.757,4,32.095v4.014H36.108V32.095C36.108,26.757,25.412,24.068,20.054,24.068Z"
+                      transform="translate(4.946 4.946)" fill="#1b254f"/>
+                <path id="MP-PATH2" data-name="MP-PATH2" d="M0,0H50V50H0Z" fill="none"/>
+            </g>
+        </g>
+    </svg>
+);
--- src/components/views/AuthForm.js
+++ src/components/views/AuthForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import {PersonIcon} from "../icons/PersonIcon";
 
 const Wrapper = styled.div`
       display: flex;
@@ -11,9 +12,24 @@ const Wrapper = styled.div`
       align-items: center;
 `;
 
+const LoginForm = styled.form`
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      padding: 5rem;
+      width: 40rem;
+      height: 47rem;
+      border-radius: 2rem;
+      background-color: white;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 30px 0 rgba(0, 0, 0, 0.19);
+`;
+
 const AuthFrom = () => (
     <Wrapper>
-        <div>Hello CODERS`PEAK!</div>
+        <LoginForm>
+            <PersonIcon width="10rem" height="10rem"/>
+        </LoginForm>
     </Wrapper>
 );
 
